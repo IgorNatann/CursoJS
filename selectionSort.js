c34089f34c4a8e3a75d0d0492b42d7f6477d7ee7
Igor Natan <igornatan4@gmail.com>
2023-08-03 10:23:22 -0300

    commit ordenação por seleção

--- selectionSort.js
+++ selectionSort.js
@@ -1,15 +1,16 @@
 const livros = require('./listaLivros');
 const menorValor = require('./menorValor');
 
-for (let atual = 0; atual < livros.length; atual++) {
+for (let atual = 0; atual < livros.length - 1; atual++) {
     let menor = menorValor(livros, atual)
 
     let livroAtual = livros[atual];
+    console.log('posição atual', atual);
     console.log('livro atual', livros[atual]);
-    let livroMenosPreco = livros[menor];
+    let livroMenorPreco = livros[menor];
     console.log('livro atual', livros[menor]);
 
-    livros[atual] = livroMenosPreco
+    livros[atual] = livroMenorPreco
     livros[menor] = livroAtual
 
 }
